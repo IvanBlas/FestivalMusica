19dfe0e582d6d679d5329aaed59d4f6d112335da
IvanBlas <estevezivan09n@hotmail.com>
2021-12-06 18:25:33 +0100

    Festival de Musica

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+const { src, dest, watch, parallel } = require('gulp');
+
+//CSS
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+const autoPrefixer = require('autoprefixer');
+const cssNano = require('cssnano');
+const postCss = require('gulp-postcss');
+const sourceMaps = require('gulp-sourcemaps');
+
+// JavaScript
+
+const terser = require('gulp-terser-js');
+
+//imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css(done){
+    
+    src('src/scss/**/*.scss')//identificar el archivo .SCSS a compilar
+        .pipe(sourceMaps.init())
+        .pipe(plumber())
+        .pipe( sass() ) //compilarlo
+        .pipe(postCss([autoPrefixer(), cssNano()]))
+        .pipe(sourceMaps.write('.'))
+        .pipe( dest('build/css') ) //almarcenarla en el disco duro 
+    done();
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionAvif( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+
+function javascript( done ) {
+    src('src/js/**/*.js')
+        .pipe(sourceMaps.init())
+        .pipe(terser())
+        .pipe(sourceMaps.write('.'))
+        .pipe(dest('build/js'));
+
+    done();
+}
+
+function dev( done ) {
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
+    done();
+}
+
+
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev) ;
+
+
+
+
+
+
+
+
+
+
+
